17046680c4d616519f9122c0eee233361cfe62c5
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2021-05-19 07:34:00 +0400

    задачи 2-5

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -9,8 +9,12 @@ function print(text) {
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
-function isValid(name) {
-  // ваш код...
+ function isValid(name) {
+  if (name && name.indexOf(' ')==-1 && name.length > 3) {
+    return true
+  } else { 
+    return false 
+  }
 }
 
 function sayHello() {
--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,5 @@
-function ucFirst(str) {
-  // ваш код...
+function ucFirst(str){
+  let upStr= str.slice(0, 1).toUpperCase() ;
+  let lowStr= str.slice(1)
+  return upStr+lowStr
 }
