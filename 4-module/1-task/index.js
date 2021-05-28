653956c0ba186a1483b8aa599a0b673a839cc040
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2021-05-28 12:47:53 +0400

    модуль 4 задачи 1-3

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
-function makeFriendsList(friends) {
-  // ваш код...
+function makeFriendsList(listFriend) {
+  let ul = document.createElement('ul');
+  for(let unit of listFriend){
+    let li = document.createElement('li')
+    li.textContent = `${unit.firstName} ${unit.lastName}`
+ul.append(li)
+}
+return ul
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
-  // ваш код...
+  Array.from(table.querySelector('tbody').rows).forEach((str, i) => {
+    let cells = str.querySelectorAll('td');
+    cells.forEach((item) => {
+      if (item.textContent === 'm') {
+        item.closest('tr').classList.add('male')
+      }
+      if (item.textContent === 'f') {
+        item.closest('tr').classList.add('female')
+      }
+      
+      if (Number(item.textContent) < 18){
+        item.closest('tr').style.cssText="text-decoration: line-through"
+      }
+    })
+    let cellsAtt = Array.from(cells).filter(el => el.hasAttribute('data-available'));
+    if (cellsAtt.length === 0) {
+      str.setAttribute('hidden', true)
+    };
+    cellsAtt.forEach((cell) => {
+      if (cell.getAttribute('data-available') === 'true') {
+        cell.closest('tr').classList.toggle('available')
+      }
+      else { cell.closest('tr').classList.toggle('unavailable') }
+    })
+
+
+  })
 }
