fe537d3bf6786ee1b38fc972ec557bb8b937bf8b
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2021-06-03 06:59:35 +0400

    модуль 5 задачи 1-3

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  function hiddens(){
+    text.hidden = !text.hidden;
+  }
+  button.addEventListener('click', hiddens)
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,32 @@
 function initCarousel() {
-  // ваш код...
+  const width = 988; // ширина картинки
+  const count = 1;
+
+  const list = document.querySelector('.carousel__inner'); //переменная ленты
+  const listElems = list.querySelectorAll('.carousel__slide');//NodeList слайдов
+  const leftBtn = document.querySelector('.carousel__arrow_left') //кнопка прокуртки влево
+  const rightBtn = document.querySelector('.carousel__arrow_right') //кнопка прокуртки вправо
+
+  leftBtn.style.display = 'none'
+
+  let position = 0;
+  leftBtn.onclick = () => {
+    rightBtn.style.display = ''
+    position += width * count;
+    position = Math.min(position, 0)
+    list.style.marginLeft = position + 'px';
+    if (position >= 0) {
+      leftBtn.style.display = 'none'
+    }
+  }
+  
+  rightBtn.onclick = () => {
+    leftBtn.style.display = ''
+    position -= width * count;
+    position = Math.max(position, -width * (listElems.length - count));
+    list.style.marginLeft = position + 'px';
+    if (-width * (listElems.length - count) / position === 1) {
+      rightBtn.style.display = 'none'
+    }
+  }
 }
